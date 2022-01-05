a9684714eec5fc02258be9ae7337c3a1c414b8e2
akhilbhadrangirija <83879587+akhilbhadrangirija@users.noreply.github.com>
2022-01-05 13:02:58 +0530

    js editted in main

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ document.getElementById("demo").onclick{
 document.getElementById("head").innerHTML="dark";
 };
 
+document.getElementById("new").onclick{
+    document.getElementById("head").innerHTML="dark";
+    };
+    
